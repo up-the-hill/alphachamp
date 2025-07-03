9d01eb0c9cf7ec26751e5d4c08dc173d3a6306c3
up-the-hill <aj.sth.2022@gmail.com>
2025-07-03 16:01:17 +1000

    tidied eval logic

--- alphachamp.py
+++ alphachamp.py
@@ -1,73 +1,75 @@
 import chess
 
 
+piece_values = {
+    chess.PAWN: 100,
+    chess.KNIGHT: 300,
+    chess.BISHOP: 350,
+    chess.ROOK: 500,
+    chess.QUEEN: 900,
+}
+
+
+def get_side_score(board: chess.Board, c: chess.Color) -> int:
+    pawn_val = len(board.pieces(chess.PAWN, c)) * piece_values[chess.PAWN]
+    knight_val = len(board.pieces(chess.KNIGHT, c)) * piece_values[chess.KNIGHT]
+    bishop_val = len(board.pieces(chess.BISHOP, c)) * piece_values[chess.BISHOP]
+    rook_val = len(board.pieces(chess.ROOK, c)) * piece_values[chess.ROOK]
+    queen_val = len(board.pieces(chess.QUEEN, c)) * piece_values[chess.QUEEN]
+
+    return pawn_val + knight_val + bishop_val + rook_val + queen_val
+    
+
+
 def eval(board: chess.Board):
     e = 0
-    piece_values = {
-        chess.PAWN: 100,
-        chess.KNIGHT: 300,
-        chess.BISHOP: 350,
-        chess.ROOK: 500,
-        chess.QUEEN: 900,
-    }
-    # loop over all piece codes
+
     # +ve eval = better position for white
     # -ve eval = worse position for black
-    for i in range(1, 6):
-        e = e + piece_values[i] * (
-            len(board.pieces(i, chess.WHITE)) - len(board.pieces(i, chess.BLACK))
-        )
+    e += get_side_score(board, chess.WHITE)
+    e -= get_side_score(board, chess.BLACK)
 
-    return e
+    # add score for having pieces closer to the center of the board
+    flip = 1 if board.turn == chess.WHITE else -1
+
+    return e * flip
 
 
 def next_move(board: chess.Board, depth=4, debug=False) -> chess.Move:
     best_move: chess.Move = chess.Move.from_uci("0000")
-    if board.turn == chess.WHITE:
-        best_eval = float("-inf")
-        for move in board.generate_legal_moves():
-            board.push(move)
-            score = minimax(board, depth - 1)
-            board.pop()
-            if score > best_eval:
-                best_eval = score
-                best_move = move
-    else:
-        best_eval = float("inf")
-        for move in board.generate_legal_moves():
-            board.push(move)
-            score = minimax(board, depth - 1)
-            board.pop()
-            if score < best_eval:
-                best_eval = score
-                best_move = move
+    max_e = -9999999
+    for move in board.generate_legal_moves():
+        board.push(move)
+        score = -negamax(board, depth - 1)
+        if score > max_e:
+            max_e = score
+            best_move = move
+        board.pop()
 
     if debug:
-        print("eval: ", best_eval)
+        print("eval: ", max_e)
         print("best_move: ", best_move)
 
     return best_move
 
 
-def minimax(board: chess.Board, depth: int) -> int:
+def negamax(board: chess.Board, depth: int) -> int:
     if depth == 0:
         return eval(board)
 
-    # find highest eval score for maximising player (white)
-    if board.turn == chess.WHITE:
-        max_e = -99999
-        for move in board.generate_legal_moves():
-            board.push(move)
-            score = minimax(board, depth - 1)
-            max_e = max(max_e, score)
-            board.pop()
-        return max_e
+    max_e = -9999999
 
-    else:
-        min_e = 99999
-        for move in board.generate_legal_moves():
-            board.push(move)
-            score = minimax(board, depth - 1)
-            min_e = min(min_e, score)
-            board.pop()
-        return min_e
+    if board.is_game_over():
+        if board.is_checkmate():
+            return -9999999
+        else:
+            return 0
+
+    for move in board.generate_legal_moves():
+        board.push(move)
+        score = -negamax(board, depth - 1)
+        if score > max_e:
+            max_e = score
+        board.pop()
+
+    return max_e
